1949f6588dc2e15e90ac302b3abb4ada356b6e49
ateals <ateals@icloud.com>
2024-05-11 01:29:45 +0900

    style(#1): input 컴포넌트 스타일 변경
    required 가 true 라면 라벨에 필수 표시
    margin 추가

--- src/feature/ui/Input.tsx
+++ src/feature/ui/Input.tsx
@@ -2,6 +2,7 @@ import { forwardRef } from "react";
 
 import { Input as ShadcnInput } from "@/feature/shadcn/components/ui/input";
 import { Label } from "@/feature/shadcn/components/ui/label";
+import { cn } from "../shadcn/utils";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -10,10 +11,13 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return (
-    <div>
-      <Label htmlFor={props.name}>{props.label}</Label>
-      <ShadcnInput id={props.name} ref={ref} {...props} />
-      <Label htmlFor={props.name} className="text-red-500">
+    <div className="my-2">
+      <Label htmlFor={props.name} className="font-bold">
+        {props.label} {props.required && <span className="text-red-500">*</span>}
+      </Label>
+      <ShadcnInput id={props.name} ref={ref} className={cn(props.className, "my-3")} {...props} />
+
+      <Label htmlFor={props.name} className="text-red-500 min-h-4 block">
         {props.error}
       </Label>
     </div>
