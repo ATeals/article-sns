99801c189103b03d722d5e2e31097d9d1438ea18
ateals <ateals@icloud.com>
2024-05-11 19:33:06 +0900

    feat: QueryProvider , ClientProvider 생성, root layout ClientProvider 할당

---
+++ src/widgets/common/ClientProvider.tsx
@@ -0,0 +1,10 @@
+import { QueryProvider } from "@/feature/common/ReactQuery/QueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryProvider>
+      {children} <ReactQueryDevtools />
+    </QueryProvider>
+  );
+};
